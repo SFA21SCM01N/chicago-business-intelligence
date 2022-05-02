211679924b7425c8b3059c3b8e5f0f9248f36893
SNETHAGANI <snethagani@hawk.iit.edu>
2022-05-01 20:42:17 -0500

    code

--- main.go
+++ main.go
@@ -194,11 +194,7 @@ func main() {
 		panic(err)
 	}
 	
-	port := os.Getenv("PORT")
-	if port == "" {
-        port = "8080"
-	}
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	
 
 	// Test the database connection
 	//err = db.Ping()
@@ -217,6 +213,11 @@ func main() {
 		GetTaxiTrips(db)
 		GetUnemploymentRates(db)
 		GetBuildingPermits(db)
+		port := os.Getenv("PORT")
+	if port == "" {
+        port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 
 		// Pull the data once a day
 		// You might need to pull Taxi Trips and COVID data on daily basis
